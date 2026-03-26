d587da385609b50cfd0539ac72ee7a9a20517f7f
__EnzoDias__ <enzodias.oficial@gmail.com>
2026-03-25 22:13:13 -0300

    Add(): function alimentar

--- Gatos/script.js
+++ Gatos/script.js
@@ -1,14 +1,14 @@
-const gato6 = document.getElementById("gato6")
+const gato = document.getElementById("gato")
 const botao = document.getElementById("cookie")
 
 const estados = {
-    normal: "/img/6.png",
-    trabalhando: "/img/1.png",
-    estudando: "/img/2.png",
-    amavel: "/img/3.png",
-    iniSono: "/img/5.png",
-    dormindo: "/img/4.png",
-    acordando: "/img/7.png",
+    normal: "img/6.png",
+    trabalhando: "img/1.png",
+    estudando: "img/2.png",
+    amavel: "img/3.png",
+    iniSono: "img/5.png",
+    dormindo: "img/4.png",
+    acordando: "img/7.png",
 }
 
 let contador = 0;
@@ -24,13 +24,28 @@ function controlador(){
             console.log("Tempo: ", contador);
 
             if (contador == 30) {
-                gato6.src = estados.iniSono
-            }
-
-            if (contador == 60) {
-                gato6.src = estados.dormindo
-            }
-    }, 100) 
+                gato.src = estados.iniSono
+            }    
+    
+            if (contador == 60) {    
+                gato.src = estados.dormindo    
+            }    
+    }, 100)     
+}    
+    
+function alimentar(){    
+    gato.src = estados.amavel    
+    contador = 0    
+    console.log("Comeu");    
+    
+    if (time_click) clearInterval(intervalo) 
+        time_click = setTimeout(() => {
+            gato.src = estados.amavel
+            
+            time_out = setTimeout(() => {
+                gato.src = estados.trabalhando
+            }, 2000)
+        }, 100)    
 }
 
 controlador();
